205df88b840033234b763e845594757c83172e39
Rohmat Dasuki <rohmat661@gmail.com>
2023-08-12 07:33:20 +0700

    update setting android and ios

--- android/app/src/main/java/com/rd/tmdbapp/MainActivity.java
+++ android/app/src/main/java/com/rd/tmdbapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.movieapp;
+package com.rd.tmdbapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "movieApp";
+    return "RD_TMDB";
   }
 }
--- ios/RDTMDBTests/RDTMDBTests.m
+++ ios/RDTMDBTests/RDTMDBTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface movieAppTests : XCTestCase
+@interface RDTMDBTests : XCTestCase
 
 @end
 
-@implementation movieAppTests
+@implementation RDTMDBTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
